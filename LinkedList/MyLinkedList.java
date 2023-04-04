b56dc959c3ab242b65e569655908234db2a71d00
rithvikch <63680838+rithvikch@users.noreply.github.com>
2023-04-04 13:23:37 -0500

    Add files via upload

---
+++ LinkedList/MyLinkedList.java
@@ -0,0 +1,200 @@
+import java.util.ArrayList;
+
+public class MyLinkedList {
+    
+    private ListNode head;
+
+    private class ListNode{
+
+        int val;
+        ListNode next;
+
+        public ListNode(int val){
+            this.val = val;
+        }
+        public String toString(){
+            return "" + this.val;
+        }
+
+
+    }
+
+    public MyLinkedList(){
+        head = null;    
+    }
+    public MyLinkedList(int val){
+        head = new ListNode(val);
+        head.next = null;
+    }
+
+    public void add(int newVal){
+        if (head!= null){
+            ListNode temp = head;
+            while (temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = new ListNode(newVal);
+        }
+        else {
+            head = new ListNode(newVal);
+        }
+
+
+    }
+
+    public boolean contains(int target){
+        ListNode temp = head;
+
+        while (temp.val != target){
+
+            if (temp.next == null){
+                return false;
+            }
+            temp = temp.next;
+
+
+        }
+        return true;    
+    }
+
+    public int get(int index){
+
+        ListNode temp = head;
+
+        for (int i = 0; i < index; i++){
+            if (temp.next == null){
+                throw new IndexOutOfBoundsException();
+            }
+            temp = temp.next;
+        }
+        return temp.val;
+    }
+    public int indexOf(int target){
+        ListNode temp = head;
+        int ind = 0;
+        while (temp.val != target){
+
+            if (temp.next == null){
+                return -1;
+            }
+            temp = temp.next;
+            ind++;
+
+        }
+        return ind;
+        
+        
+
+    }
+    public void set(int newVal, int index){
+
+        ListNode temp = head;
+
+        for (int i = 0; i < index; i++){
+            if (temp.next == null){
+                throw new IndexOutOfBoundsException();
+            }
+            temp = temp.next;
+            
+        }
+        temp.val = newVal;
+        
+        
+
+
+
+
+
+    }
+    public int size(){
+        ListNode temp = head;
+        int size = 0;
+        while (temp != null){
+            temp = temp.next;
+            size++;
+        }
+        return size;
+
+    }
+    public int sizeRecursive(ListNode cur){
+
+        if (cur.next == null){
+            return 0;
+        }
+        return 1 + sizeRecursive(cur.next);
+
+    }
+
+    public boolean isEmpty(){
+        return head == null;
+    }
+
+    public int remove(int index){
+        ListNode temp = head;
+
+        if (temp != null && index == 0){
+            int val = temp.val;
+            head = temp.next;
+            return val;
+        }
+
+        for (int i = 0; i < index-1; i++){
+            if (temp.next == null){
+                throw new IndexOutOfBoundsException();
+            }
+            temp = temp.next;
+            
+        }
+        int val = temp.next.val;
+       
+        temp.next = temp.next.next;
+       
+        return val;
+    }
+
+    public void add(int newVal, int index){
+        ListNode temp = head;
+        for (int i = 0; i < index-1; i++){
+            if (temp == null){
+                throw new IndexOutOfBoundsException();
+            }
+            temp = temp.next;
+            
+        }
+        ListNode connect = temp.next;
+        temp.next = new ListNode(newVal);
+        temp.next.next = connect;
+
+
+
+
+    }
+    @Override
+    public String toString(){
+        ArrayList<Integer> stuff = new ArrayList<>();
+        String msg = "";
+        if (isEmpty()){
+            msg = "[]";
+        }
+        else {
+            msg = "";
+            ListNode temp = head;
+            
+            while (temp != null){
+                stuff.add(temp.val);
+                temp = temp.next;
+                
+            }
+            msg = stuff.toString();
+           
+            
+        }
+        return msg;
+        
+    }
+
+
+
+
+
+}
